86f8da14b683840adb6f7d0f6fbd9b3131c26838
Lisa <lisa.baiden26@gmail.com>
2025-04-10 18:10:51 +0000

    index file has been updated

---
+++ index.js
@@ -0,0 +1,45 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import errorHandler from "./middlewares/errorHandler.js"
+
+// importing routes
+import authRoutes from "./routes/authRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import menuRoutes from "./routes/menuRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import reservationRoutes from "./routes/reservationRoutes.js";
+import loyaltyRoutes from "./routes/loyaltyRoutes.js";
+import inventoryRoutes from "./routes/inventoryRoutes.js";
+import deliveryRoutes from "./routes/deliveryRoutes.js";
+import analyticsRoutes from "./routes/analyticsRoutes.js";
+
+
+// making a database connection
+await mongoose.connect(process.env.MONGO_URI);
+
+// create an express app
+const app = express();
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+
+// using routes
+app.use(authRoutes);
+app.use(userRoutes)
+app.use(menuRoutes)
+app.use(orderRoutes)
+app.use(reservationRoutes)
+app.use(loyaltyRoutes)
+app.use(analyticsRoutes)
+
+app.use(errorHandler)
+
+// server listening
+const port = process.env.PORT || 4512
+app.listen(port, () => {
+  console.log(`Hearts and Plates is ready to serve on port ${ port}`);
+});
