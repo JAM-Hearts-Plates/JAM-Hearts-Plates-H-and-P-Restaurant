50274936f17ceb61a53c3c14428a46851ab85f9b
Lisa <lisa.baiden26@gmail.com>
2025-04-24 01:12:36 +0000

    google maps

--- services/map.js
+++ services/map.js
@@ -10,3 +10,12 @@ export const calculateDistance = (location1, location2) => {
   const distance = haversine(location1, location2); // Calculate distance in meters
   return distance / 1000; // Convert meters to kilometers
 };
+
+// google maps geolocation
+const apiKey = process.env.GOOGLE_MAPS_API_KEY;
+
+// For server-side geocoding if needed
+export const geocodeAddress = async (address) => {
+  const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`);
+  return response.json();
+};
